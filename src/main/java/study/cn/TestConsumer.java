103977bd116020355f7ec4c1113df0f588ad0b6b
wulh <wulh@darenloan.com>
2019-03-01 16:54:51 +0800

    整合activeMQ，添加消费者测试代码

---
+++ src/main/java/study/cn/TestConsumer.java
@@ -0,0 +1,33 @@
+package study.cn;
+
+//消费者测试
+public class TestConsumer {
+    public static void main(String[] args) {
+        Comsumer comsumer = new Comsumer();
+        comsumer.init();
+        TestConsumer testConsumer = new TestConsumer();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+    }
+
+    private class ConsumerMq implements Runnable {
+        Comsumer comsumer;
+
+        public ConsumerMq(Comsumer comsumer) {
+            this.comsumer = comsumer;
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    comsumer.getMessage("Jaycekon-MQ");
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
