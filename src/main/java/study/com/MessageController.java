50dc23d6671e41eec2191d45ae7d6137226515f6
wulh <wulh@darenloan.com>
2019-03-25 11:22:21 +0800

    整合activeMQ，添加消费者测试代码

---
+++ src/main/java/study/com/MessageController.java
@@ -0,0 +1,51 @@
+package study.com;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import study.cn.ConsumerService;
+import study.cn.ProducerService;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.TextMessage;
+
+/**
+ * Created by Administrator on 2017/1/5.
+ */
+@Controller
+public class MessageController {
+    private Logger logger = LoggerFactory.getLogger(MessageController.class);
+//    @Resource(name = "defaultDestination")
+    @Resource(name = "demoQueueDestination")
+    private Destination destination;
+
+    //队列消息生产者
+    @Resource(name = "producerService")
+    private ProducerService producer;
+
+    //队列消息消费者
+    @Resource(name = "consumerService")
+    private ConsumerService consumer;
+
+    @RequestMapping(value = "/SendMessage")
+    @ResponseBody
+    public void send(String msg) {
+        logger.info(Thread.currentThread().getName()+"------------send to jms Start");
+        producer.sendMessage(msg);
+        logger.info(Thread.currentThread().getName()+"------------send to jms End");
+    }
+
+    @RequestMapping(value= "/ReceiveMessage")
+    @ResponseBody
+    public Object receive(){
+        logger.info(Thread.currentThread().getName()+"------------receive from jms Start");
+        TextMessage tm = consumer.receive(destination);
+        logger.info(Thread.currentThread().getName()+"------------receive from jms End");
+        return tm;
+    }
+
+}
